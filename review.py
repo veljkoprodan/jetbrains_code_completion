2ff48515d089ba608d74ea25667edbb96ff4be82
veljkoprodan <veljkoprodan@yahoo.com>
2024-11-04 04:30:49 +0100

    Add dataset with metrics json

--- review.py
+++ review.py
@@ -1,24 +1,31 @@
 import json
 from termcolor import colored
 
-with open('./json/dataset_512_output_params.json', 'r') as file:
+with open('./json/dataset_reviewed.json', 'r') as file:
     dataset = json.load(file)
     
 for i, data in enumerate(dataset, start=1):
     print(f'{i}/{len(dataset)}')
     print('=' * 100)
+    
     print(data['prefix'])
     print('-' * 50)
+    
     print(colored(data['middle'], 'green'))
     print('*' * 20)
     print(colored(data['middle_predicted'], 'yellow'))
     print('-' * 50)
+    
     print(data['suffix'], '\n')
     
     print('~' * 50)
     print('Is the predicted code correct?\n(1 - completely incorrect, 5 - perfect)')
     data['review_label'] = input()
+
+    print('Comment:')
+    data['comment'] = input()
     print('~' * 40)
+    
     print('\n')
     print('=' * 100)
     
